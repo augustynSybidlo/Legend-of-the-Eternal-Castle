246769e01a83698753f7499bda4ecd8e86149a1b
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2017-08-31 10:35:41 +0200

    inventory module created, tested - working

---
+++ inventory.py
@@ -0,0 +1,37 @@
+import random_item
+
+
+def add_to_inventory(inventory):
+    max_weight_of_inventory = 100
+    weight_of_inventory = 0
+    new_item = random_item.main()
+    for i in inventory:
+        weight_of_inventory += i[2] 
+    weight_of_inventory += new_item[2]
+    if weight_of_inventory >= max_weight_of_inventory:
+        print("You have not enought space in your inventory!")
+    else:
+        inventory.append(new_item)
+    return weight_of_inventory, inventory
+
+
+def show_inventory(inventory):
+    weight_of_items, inventory = add_to_inventory(inventory)
+    width_1 = 7
+    width_2 = 10
+    width_3 = 35
+    print("Inventory:\n")
+    print("Weight".ljust(width_1), "Type".ljust(width_2), "Item name".ljust(width_3), "\n")
+    for element in inventory:
+        print(str(element[2]).ljust(width_1), element[1].ljust(width_2), element[0].ljust(width_3))
+    print("Total weight of items: %d".rjust(width_3) % weight_of_items)
+
+
+def main(run=1):
+    if run == 0:
+        inventory = []
+    # if run != 0:
+    add_to_inventory(inventory)
+    show_inventory(inventory)
+
+main(0)
